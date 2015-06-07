94aa56eaed0ee3cd1c961017b5e03f0bc8f6fbc0
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2015-06-08 02:24:18 +0900

    add player class

---
+++ blackjack/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    class Player
+    {
+        private string name;
+        private string[] cards;
+
+        public Player(string name,string[] cards)
+        {
+            this.name = name;
+            this.cards = cards;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string[] GetCards()
+        {
+            return cards;
+        }
+    }
+}
