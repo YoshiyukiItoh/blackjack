189f5f7b0470f745ec62e7ba2647fa4e5815bf1a
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2015-06-08 04:08:28 +0900

    addclass

---
+++ blackjack/BlackJackPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace blackjack
+{
+    class BlackJackPlayer : Player
+    {
+        private int maxScore;
+
+        public BlackJackPlayer(string name, List<string> cardList)
+            : base(name, cardList)
+        {
+            SetMaxScore();
+        }
+
+        private void SetMaxScore()
+        {
+        }
+    }
+}
