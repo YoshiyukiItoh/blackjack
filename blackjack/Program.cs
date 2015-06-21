56f73c46069d8b23f2ef3d51e6f1961e1da1a883
yoshiyuki itoh <swallows.vct1a19@gmail.com>
2015-06-22 07:24:36 +0900

    クラス名を修正

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -13,8 +13,8 @@ namespace blackjack
             players.Add(new Player("player1"));
             players.Add(new Player("player2"));
 
-            Main main = new Main(new Blackjack(players)
-                                ,new DatabasePersistence());
+            ServiceController main = new ServiceController(new Blackjack(players)
+                                                          ,new DatabasePersistence());
             main.Start();
             Console.ReadLine();
 		}
