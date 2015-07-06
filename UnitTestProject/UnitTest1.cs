edf646ac90cbde8bbed7097f553c7255f936e503
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2015-07-07 00:22:04 +0900

    add testproj

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blackjack;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
