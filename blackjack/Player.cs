a3280743012e46dbc875af484f3600d56e762c2d
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2015-07-21 01:18:56 +0900

    const削除

--- blackjack/Player.cs
+++ blackjack/Player.cs
@@ -8,7 +8,7 @@ namespace blackjack
     public class Player
     {
         private const int borderline_point = 21;
-        private const Dictionary<string, int> pointTable = new Dictionary<string, int>()
+        private Dictionary<string, int> pointTable = new Dictionary<string, int>()
 		{
 			{"A", 1},{"2", 2},{"3", 3},{"4", 4},
 			{"5", 5},{"6", 6},{"7", 7},{"8", 8},
