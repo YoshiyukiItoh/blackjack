847b93b81e18e61848d700a2a38ce443523d08d3
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2015-07-21 01:22:37 +0900

    CreateACombiPatternのテストケース追加

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -22,4 +22,68 @@ namespace UnitTestProject
             Assert.AreEqual(name, str);
         }
     }
+
+    [TestClass]
+    public class UnitTestPlayerClass
+    {
+        [TestMethod]
+        public void TestMethod_CreateACombiPattern1()
+        {
+            Player p = new Player("p");
+            var pObj = new PrivateObject(p);
+
+            var actualList = (List<int>)pObj.Invoke("CreateACombiPattern", new Object[] { 1 });
+            actualList.Sort();
+            var expectedList = new List<int>() {1,11};
+            for(int i = 0;i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], actualList[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_CreateACombiPattern2()
+        {
+            Player p = new Player("p");
+            var pObj = new PrivateObject(p);
+
+            var actualList = (List<int>)pObj.Invoke("CreateACombiPattern", new Object[] { 2 });
+            actualList.Sort();
+            var expectedList = new List<int>() { 2, 12 };
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], actualList[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_CreateACombiPattern3()
+        {
+            Player p = new Player("p");
+            var pObj = new PrivateObject(p);
+
+            var actualList = (List<int>)pObj.Invoke("CreateACombiPattern", new Object[] { 3 });
+            actualList.Sort();
+            var expectedList = new List<int>() { 3, 13 };
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], actualList[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_CreateACombiPattern4()
+        {
+            Player p = new Player("p");
+            var pObj = new PrivateObject(p);
+
+            var actualList = (List<int>)pObj.Invoke("CreateACombiPattern", new Object[] { 4 });
+            actualList.Sort();
+            var expectedList = new List<int>() { 4, 14 };
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], actualList[i]);
+            }
+        }
+    }
 }
